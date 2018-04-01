a01b762d137c86748f14e78d9275c7883d22d920
Jameson Cotter <jameson.cotter@gmail.com>
2018-04-01 17:18:40 -0400

    Update HoldEm5Card, add calculator class.

--- HoldEm5Card.py
+++ HoldEm5Card.py
@@ -1,7 +1,9 @@
 from Card import HoldEmCard
-from Hand import HoldEmHand
+from Hand import HoldEmHand, Community
 from Deck import PokerDeck
 from Shoe import HoldEmShoe
+from Calculator import HoldEmCalculator
+from classifiers import check_straight_and_flush
 
 if __name__ == '__main__':
     def get_shoe(shoe, card, deck, deck_count=1):
@@ -14,21 +16,21 @@ if __name__ == '__main__':
 
     hands = [shoe.deal_hand_from_tuples(HoldEmHand, cards[0], cards[1]) for cards in \
         [
-            [('A', 'h'), ('K', 'h')],
-            [(6, 'd'), (6, 'c')],
-            [(9, 's'), ('T', 's')]
+            [('T', 'h'), ('J', 'h')],
+            [('J', 'd'), (2, 'd')],
+            [(2, 's'), (3, 's')]
         ]
-    ]
+    ]   
 
-    print([hand.to_string() for hand in hands])
+    print([hand.sort_by_pip().to_string() for hand in hands])
 
+    # community = Community(*shoe.take_from_top(5))
+    community = Community(*shoe.deal_specific(('Q', 'h'), ('K', 'h'), ('A', 'h'), (5, 'd'), (7, 'h')))
+
+    print(community.to_string())
+
+    calculator = HoldEmCalculator(hands, community)
+    winning_hand = calculator.winner(check_straight_and_flush)
+
+    print(winning_hand.to_string() if winning_hand else "no winner")
     # classifiers
-
-
-    # ranks
-
-
-    # argument parser
-
-
-    # client/orchestration
