446ffd5fe082b245c59b81e72e0fbe2484608045
Jameson Cotter <jameson.cotter@gmail.com>
2018-04-01 17:17:20 -0400

    Update utils with hand_type validator.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from Card import Card
 from Deck import Deck
+from Hand import Hand
 
 def validate_cards_type(cards=None):
     if cards is not None:
@@ -17,4 +18,10 @@ def validate_callable_type(funcs=None):
     if funcs is not None:
         for func in funcs:
             if not callable(func):
+                raise Exception
+
+def validate_hand_type(*args):
+    for hand in args:
+        if hand is not None:
+            if not isinstance(hand, Hand):
                 raise Exception
