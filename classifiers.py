fe3e0de6669c3da7f4047ff1697bc4fa4f983025
Jameson Cotter <jameson.cotter@gmail.com>
2018-04-01 19:26:28 -0400

    Update classifiers, add check_pairs.

--- classifiers.py
+++ classifiers.py
@@ -49,3 +49,36 @@ def check_straight_and_flush(hand):
         return ''.join(straight)
 
     return "0"
+
+
+
+
+def check_pairs(hand):
+    def get_card_value(dbl_dict):
+        return lambda x, y: str(dbl_dict[x][y]).zfill(2)
+    
+    pips = list([card.pip for card in hand.cards])
+
+    pairs = defaultdict(int)
+    for pip in pips:
+        pairs[pip] += 1
+
+    pair_counts = list(pairs.items())
+    pair_counts.sort(key=lambda x: (x[1], x[0]), reverse=True)
+
+    get_card = get_card_value(pair_counts)
+
+    if pair_counts[0][1] == 4:
+        return ''.join(['h', get_card(0, 0), get_card(1, 0)])
+    elif pair_counts[0][1] == 3:
+        if pair_counts[1][1] >= 2:
+            return ''.join(['g', get_card(0, 0), get_card(1, 0)])
+        else:
+            return ''.join(['d', get_card(0, 0), get_card(1, 0), get_card(2, 0)])
+    elif pair_counts[0][1] == 2:
+        if pair_counts[1][1] == 2:
+            return ''.join(['c', get_card(0, 0), get_card(1, 0), get_card(2, 0)])
+        else:
+            return ''.join(['b', get_card(0, 0), get_card(1, 0), get_card(2, 0), get_card(3, 0)])
+    else:
+        return ''.join(['a', get_card(0, 0), get_card(1, 0), get_card(2, 0), get_card(3, 0), get_card(4, 0)])
