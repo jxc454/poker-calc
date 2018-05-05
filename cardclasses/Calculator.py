deb71c45732b1aec16fbaa9f10cc69d4e95b7c58
unknown <jameson.cotter@gmail.com>
2018-05-05 19:45:45 -0400

    Update Calculator.py, refactor so calculator no longer joins hands and community cards.  It only runs classifiers and returns the winning hand.

--- cardclasses/Calculator.py
+++ cardclasses/Calculator.py
@@ -3,27 +3,22 @@ from abc import ABC
 import cardclasses.utils as utils
 
 class Calculator(ABC):
-    def __init__(self, hands, community, hand_class):
-        utils.validate_hand_type(*hands)
-        utils.validate_hand_type(community)
-
+    def __init__(self, hands):
+        self._hands = None
         self.hands = hands
-        self.community = community
-        self.hand_class  = hand_class
 
     def winner(self, *args):
         utils.validate_callable_type
 
         scores = {}
 
-        for hand in self.hands:
+        for hand in self._hands:
             best_hands = []
-            all_cards = hand.cards + self.community.cards
             for func in args:
-                best_hands.append(func(self.hand_class(*all_cards)))
+                best_hands.append(func(hand))
                 
             best_hand = max(best_hands)
-            scores[hand] = best_hand
+            scores[hand.pocket.to_string()] = best_hand
 
         max_hand = max(scores.values())
 
@@ -36,10 +31,19 @@ class Calculator(ABC):
                 winner = key
 
         return winner if winners == 1 else None
-        
+    
+    @property
+    def hands(self):
+        return self._hands
+
+    @hands.setter
+    def hands(self, hands):
+        utils.validate_hand_type(*hands)
+        self._hands = hands
+
 class HoldEmCalculator(Calculator):
-    def __init__(self, hands, community, hand_class):
-        super().__init__(hands, community, hand_class)
+    def __init__(self, hands):
+        super().__init__(hands)
 
     def winner(self, *args):
         return super().winner(*args)
